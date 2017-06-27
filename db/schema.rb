d1dd282aa368aef63da51ae7a3d89bcb99538ad7
LeTanDuoc <techinfor366@gmail.com>
2017-06-27 09:26:33 +0700

    signup

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,12 @@ ActiveRecord::Schema.define(version: 20170626091848) do
     t.string "name"
     t.string "course"
     t.integer "level"
+    t.integer "lesson_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_categories_on_user_id"
     t.index ["lesson_id"], name: "index_categories_on_lesson_id"
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "lessons", force: :cascade do |t|
@@ -34,15 +36,19 @@ ActiveRecord::Schema.define(version: 20170626091848) do
 
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
-    t.integer "followed_id"
+    t.integer "following_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["follower_id", nil], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index [nil], name: "index_relationships_on_followed_id"
   end
 
   create_table "results", force: :cascade do |t|
     t.integer "Score"
     t.string "learned_word"
     t.string "int"
+    t.integer "lesson_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["lesson_id"], name: "index_results_on_lesson_id"
@@ -62,6 +68,7 @@ ActiveRecord::Schema.define(version: 20170626091848) do
     t.string "name"
     t.string "wordlist_learned"
     t.string "wordlist_unlearned"
+    t.integer "categories_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["categories_id"], name: "index_wordlists_on_categories_id"
@@ -70,7 +77,7 @@ ActiveRecord::Schema.define(version: 20170626091848) do
   create_table "words", force: :cascade do |t|
     t.boolean "leaned_state"
     t.integer "level"
-    t.integer "wordlist_id"
+    t.integer "answer"
     t.integer "categories_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
