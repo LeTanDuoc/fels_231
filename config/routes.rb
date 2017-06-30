2bee60600ebedad74368ec9d6a7f7b8891eaa4cc
LeTanDuoc <techinfor366@gmail.com>
2017-06-30 22:06:43 +0700

    login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get "users/show"
   get "/help", to: "static_pages#help"
   get "/signup", to: "users#new"
+  get    "/login",   to: "login#new"
+  post   "/login",   to: "login#create"
+  delete "/logout",  to: "login#destroy"
   root "static_pages#home"
   get  "/signup",  to: "users#new"
   resources :users
